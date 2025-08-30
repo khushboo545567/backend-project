fe829b557ba98f215731572164d10f730d7a85df
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-08-30 23:13:37 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import dotenv from "dotenv";
 import connectionDB from "./db/dbConnection.js";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
-connectionDB();
+
+connectionDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`✅ Server is running on port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("❌ MongoDB connection failed:", error);
+  });
