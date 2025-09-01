0249c6aa05e42b4eea46843d28989775a48577a5
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-09-02 02:02:38 +0530

    created models

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -49,5 +49,21 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+// this is the middle ware by which we are storing the hashed data in to the pasword and generate the token as well
+// THIS PRE FUNCTION WILL RUN AFTER EVERY CHANGE IN THE DB SO TO STOP THAT USE ISMODIFIED FUNCTIon
+// SIGNUP -------------
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+// LOGIN---------------------------
+// CHEK THE USER SEND THE CORRECT PASSWORD OR NOT
+userSchema.methods.ischeckPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// JWT TOKEN GENERATE OF EVERY REQUEST FORM THE CLIENT-----------
 
 export const User = mongoose.model("User", userSchema);
